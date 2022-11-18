5df04607c297e8e54c8e7aa14a5281a01c73c8df
RustamovVugar <vugarar@code.edu.az>
2022-11-18 23:09:47 +0400

    added task2 folder

---
+++ ListBook/ListBook/Models/Book.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ListBook.Models
+{
+    class Book
+    {
+        public string Name { get; set; }
+        public string Authoru { get; set; }
+        public double Price { get; set; }
+
+    }
+}
---
+++ ListBook/ListBook/Program.cs
@@ -0,0 +1,24 @@
+﻿using ListBook.Models;
+using System;
+using System.Collections.Generic;
+
+namespace ListBook
+{
+    class Program
+    {
+        public delegate bool Check(string str);
+        static void Main(string[] args)
+        {
+            List<Book> pages = new List<Book>();
+
+            pages.Add(new Book { Name = "C#", Authoru = "Nizami", Price = 50 });
+            pages.Add(new Book { Name = "JavaScript", Authoru = "Vugar",  Price = 130 });
+            pages.Add(new Book { Name = "CSS", Authoru = "Nizami",  Price = 45 });
+            pages.Add(new Book { Name = "HTML", Authoru = "Nizami", Price = 120 });
+            pages.Add(new Book { Name = "C++",Authoru = "Nizami",  Price = 55 });
+
+            var result = pages.FindAll(m => m.Authoru == "Nizami");
+            Console.WriteLine(result.Count);
+        }
+    }
+}
